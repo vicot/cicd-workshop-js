913b9ab29c7f55cbb728d1ae5ddb28a5755a09e4
vicot <tarkamo@gmail.com>
2021-03-22 19:29:47 +0000

    c

--- send.sh
+++ send.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo curl --location -v --request POST 'http://f126c4922131.ngrok.io' --header 'Content-Type: application/json' --data-raw '{"circleci-build-url": "'$CIRCLE_BUILD_URL'"}'
+echo ""
 curl --location -v --request POST 'http://f126c4922131.ngrok.io' --header 'Content-Type: application/json' --data-raw '{"circleci-build-url": "'$CIRCLE_BUILD_URL'"}'
